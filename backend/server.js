c1a6f4cd0212ee19b2dbabe6d6f2222556f16044
Neeshita <neeshita108@gmail.com>
2026-06-29 19:20:36 +0530

    fix cors origin wildcard

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(cors({
   origin: [
-    'http://localhost:5173',
+
     'https://blogplatform-rust.vercel.app/', // update after you get vercel URL
   ],
   credentials: true,
